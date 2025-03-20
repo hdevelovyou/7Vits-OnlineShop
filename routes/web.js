d1f36b7813ab4533007e392b20e95263ad6d1a23
BlankBire <165353658+BlankBire@users.noreply.github.com>
2025-03-20 09:42:02 +0700

    Delete routes directory

--- routes/web.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {getHomePage, getColorGalaxy} = require('../controllers/HomeController');
-const router = express.Router();
-
-router.get('/', getHomePage);
-router.get('/ColorGalaxy', getColorGalaxy);
-
-module.exports = router; 
