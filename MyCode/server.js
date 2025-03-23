52091b513f7cd18c293e9651b6f05771bbf8a3bf
BlankBire <nguyenhuucanhnc2005@gmail.com>
2025-03-23 11:12:59 +0700

    update login FE + BE

--- MyCode/server.js
+++ MyCode/server.js
@@ -9,22 +9,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/', authRoutes);
-
-// Serve static files từ thư mục 'public'
-app.use(express.static(path.join(__dirname, 'public')));
-// Định tuyến để loại bỏ .html trong URL
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'register.html'));
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
-
-app.get('/logout', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'logout.html'));
-});
+app.use('/api/auth', authRoutes); // Đổi URL API để tránh trùng với route frontend
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
