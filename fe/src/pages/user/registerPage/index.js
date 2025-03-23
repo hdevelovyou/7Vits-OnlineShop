bb48e2a56b3aec8c92412aaaf085312771d73825
BlankBire <nguyenhuucanhnc2005@gmail.com>
2025-03-24 02:30:02 +0700

    Your commit message

--- fe/src/pages/user/registerPage/index.js
+++ fe/src/pages/user/registerPage/index.js
@@ -9,69 +9,69 @@ const RegisterPage = () => {
     return (
         <div className="signup-page">
             <div className="form-sign">
-                    <div className="title">
-                        <h1>Đăng ký tài khoản</h1>
-                    </div>
-                    <div className="input-group">
-                        <input
-                            type="fname"
-                            id="fname"
-                            name="fname"
-                            placeholder="Họ"
-                        />
-                        <input
-                            type="lname"
-                            id="lname"
-                            name="lname"
-                            placeholder="Tên"
-                        />
-                    </div>
-                    <div className="input-group">
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            placeholder="Email / SĐT"
-                        />
-                    </div>
-                    <div className="input-username">
-                        <FaCircleUser className="icon" />
-                        <input
-                            type="text"
-                            id="username"
-                            name="username"
-                            placeholder="Tên tài khoản"
-                        />
-                    </div>
-                    <div className="input-group">
-                        <FaLock className="icon" />
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            placeholder="Mật khẩu"
-                        />
-                        <input
-                            type="confirmPassword"
-                            id="conformPassword"
-                            name="conformPassword"
-                            placeholder="Xác nhận mật khẩu"
-                        />
-                    </div>
+                <div className="title">
+                    <h1>Đăng ký tài khoản</h1>
+                </div>
+                <div className="input-group">
+                    <input
+                        type="fname"
+                        id="fname"
+                        name="fname"
+                        placeholder="Họ"
+                    />
+                    <input
+                        type="lname"
+                        id="lname"
+                        name="lname"
+                        placeholder="Tên"
+                    />
+                </div>
+                <div className="input-group">
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Email / SĐT"
+                    />
+                </div>
+                <div className="input-username">
+                    <FaCircleUser className="icon" />
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder="Tên tài khoản"
+                    />
+                </div>
+                <div className="input-group">
+                    <FaLock className="icon" />
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Mật khẩu"
+                    />
+                    <input
+                        type="confirmPassword"
+                        id="conformPassword"
+                        name="conformPassword"
+                        placeholder="Xác nhận mật khẩu"
+                    />
+                </div>
 
                 <div className="signup">
-                        <div className="otherSignUp">
-                            <FaFacebook className="icon" />
-                            <FcGoogle className="icon-gg" />
-                        </div>
-    
-                        <div className="sig-btn">
-                            <Link to="/otp-for-signup"><button type="button">Đăng ký</button></Link>
-                        </div>
+                    <div className="otherSignUp">
+                        <FaFacebook className="icon" />
+                        <FcGoogle className="icon-gg" />
+                    </div>
+
+                    <div className="sig-btn">
+                        <Link to="/otp-for-signup"><button type="button">Đăng ký</button></Link>
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
 
-export default memo (RegisterPage);
+export default memo(RegisterPage);
