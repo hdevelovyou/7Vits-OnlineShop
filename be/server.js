75ea3afdab193f0770ebf0cd3170fef65f910db7
Châu Hoàng Phúc <144977826+chauhoangphuc05@users.noreply.github.com>
2025-03-28 23:58:53 +0700

    no roi cac chau oi

--- be/server.js
+++ be/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const env = require('./config/enviroment'); // Use the environment config
 const passport = require('./config/passport');
 const db = require('./config/connectDB');
-
+const axios = require('axios');
 const app = express();
 
 // Middleware
@@ -39,8 +39,23 @@ app.get('/api/test', (req, res) => {
   res.json({ message: 'API is working', env: env.GOOGLE_CLIENT_ID ? 'OAuth configured' : 'OAuth not configured' });
 });
 
+app.use(cors()); 
+
+app.get('/api/products', async (req, res) => {
+  try {
+      const response = await axios.get('https://divineshop.vn/api/product/list?limit=24&tag=Gi%E1%BA%A3i+tr%C3%AD');
+      res.json(response.data);
+  } catch (error) {
+      console.error('Lỗi khi lấy dữ liệu:', error.message);
+      res.status(500).json({ error: 'Lỗi khi lấy dữ liệu từ API.' });
+  }
+});
+
+
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
