01aef3236f651c5c3e6444d38f1849ed7e593725
Châu Hoàng Phúc <144977826+chauhoangphuc05@users.noreply.github.com>
2025-05-08 17:11:50 +0700

    fe

--- be/server.js
+++ be/server.js
@@ -29,6 +29,16 @@ const io = socketIO(server, {
   }
 });
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "React app URL"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
