62187251102ab282d3aca1486dc3c001924b3931
Fin-tan <23521080@gm.uit.edu.vn>
2025-06-04 11:54:11 +0700

    fix

--- fe/src/components/socket.js
+++ fe/src/components/socket.js
@@ -1,9 +1,9 @@
 // src/socket.js
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000', {
+const socket = io(process.env.REACT_APP_API_URL, {
   withCredentials: true,
-  autoConnect: true,    // sẽ connect thủ công khi cần
+//  autoConnect: true,    // sẽ connect thủ công khi cần
 });
 
 export default socket;
